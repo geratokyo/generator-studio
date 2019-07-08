9e6a9f6a75922d936492300f57aec113e74b106a
Falcon <gera@vervesearch.com>
2019-07-08 12:00:48 +0100

    add function to get the user's browser

--- app/templates/src/config.ts
+++ app/templates/src/config.ts
@@ -59,3 +59,32 @@ export const IS_MOBILE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberr
  * if browser is safari
  */
 export const IS_SAFARI = navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf('Chrome') == -1;
+
+
+export const GET_BROWSER = () => {
+    var wBrowser, sUsrAg = navigator.userAgent;
+    // The order matters here, and this may report false positives for unlisted browsers.
+    if (sUsrAg.indexOf("Firefox") > -1) {
+        wBrowser = "moz";
+        // "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+    } else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
+        wBrowser = "opr";
+        //"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106"
+    } else if (sUsrAg.indexOf("Trident") > -1) {
+        wBrowser = "ie";
+        // "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko"
+    } else if (sUsrAg.indexOf("Edge") > -1) {
+        wBrowser = "ed";
+        // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+    } else if (sUsrAg.indexOf("Chrome") > -1) {
+        wBrowser = "chr";
+        // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
+    } else if (sUsrAg.indexOf("Safari") > -1) {
+        wBrowser = "saf";
+        // "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
+    } else {
+        wBrowser = "unk";
+    }
+    console.log('wBrowser', wBrowser);
+    return wBrowser;
+}
