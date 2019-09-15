79773edb495849a8fb04c20fee86ffe4d22d6a91
Simeon <simeoncoding@gmail.com>
2019-09-15 17:49:46 +0100

    fix: remove typo from file

--- app/templates/src/services/Firebase.ts
+++ app/templates/src/services/Firebase.ts
@@ -13,4 +13,4 @@ export const FIREBASE_CONF = {
     messagingSenderId: ""
 }
 
-export default !firebase.apps.length ? firebase.initializeApp(FIREBASE_CONF) : firebase.app();s
+export default !firebase.apps.length ? firebase.initializeApp(FIREBASE_CONF) : firebase.app();
