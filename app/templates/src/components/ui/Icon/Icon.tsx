ed97bdd086b754901444a500bf4a4561ea72d7f9
Simeon <simeoncoding@gmail.com>
2019-09-15 17:09:05 +0100

    Did format document on all files and end up with pretty much these results. Some very minimal were left behind. (Only format document was made on these files & no other changes)

--- app/templates/src/components/ui/Icon/Icon.tsx
+++ app/templates/src/components/ui/Icon/Icon.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react'; 
+import * as React from 'react';
 
 
-export interface IconProps{
-    className?:string;
-    onClick?:()=>void;
+export interface IconProps {
+    className?: string;
+    onClick?: () => void;
 }
 
-export interface IconState{
+export interface IconState {
 
 }
 
-export const Icon:React.SFC<IconProps> = (props)=>{
-    let  cls = props.className || "";
+export const Icon: React.SFC<IconProps> = (props) => {
+    let cls = props.className || "";
     return (
         <div className={"icon " + cls} onClick={props.onClick && props.onClick}>
             {props.children}
