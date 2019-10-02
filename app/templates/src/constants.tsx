f9b05f02219755b9e65b301edb36232f6d87c91e
nickVerve <nick@vervesearch.com>
2019-10-02 11:43:39 +0100

    Add addPostFix function

--- app/templates/src/constants.tsx
+++ app/templates/src/constants.tsx
@@ -2,33 +2,41 @@ import { DialogProvider } from "./components/ui/Dialog/DialogProvider";
 
 export const APP_START      	= 0x0100000;
 
-
 export const DP = new DialogProvider();
 
-
-
-export const NUMBER_FORMAT_FUNCTION= {
-    numberWithCommas: (x)=>{
+export const NUMBER_FORMAT_FUNCTION = {
+    numberWithCommas: (x) => {
         let s: string = ".";
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, s);
     },
-    currencyNumber: (x, symbol)=>{
+    currencyNumber: (x, symbol) => {
         x=parseInt(x).toFixed(0);
         return x === "£-" ? "N/A": (x < 0 ? "-":"") + symbol + Math.abs(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    }, 
-    decimal:(x, decimalPoints)=>{
+    },
+    decimal: (x, decimalPoints) => {
         return parseFloat(x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")).toFixed(decimalPoints);
-    }, 
-    percent:(x)=>{
-        x = x*100;
+    },
+    percent: (x) => {
+        x = x * 100;
         return (x).toFixed(x < 1 ? 2 : x < 10 ? 1 : 0) + "%";
     },
-    seCurrency:(x, symbol)=>{
-        x=parseInt(x).toFixed(0);
+    seCurrency: (x, symbol) => {
+        x = parseInt(x).toFixed(0);
         return x === "£-" ? "N/A": (x < 0 ? "-":"+") + symbol + Math.abs(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     },
-    reverseCurrencyNumber:function(x, symbol){
+    reverseCurrencyNumber: (x, symbol) => {
         x=parseInt(x).toFixed(0);
         return Math.abs(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + symbol;
+    },
+    addPostFix: (labelValue: number, decimalPlaces: number, label: string) => {
+        return Math.abs(Number(labelValue)) >= 1.0e+9
+            ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(decimalPlaces) + label
+            // Six Zeroes for Millions
+            : Math.abs(Number(labelValue)) >= 1.0e+6
+                ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(decimalPlaces) + label
+                // Three Zeroes for Thousands
+                : Math.abs(Number(labelValue)) >= 1.0e+3
+                    ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(decimalPlaces) + label
+                    : Math.abs(Number(labelValue));
     }
 }
